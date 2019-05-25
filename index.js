7564592b9dc5c0ffae8b39bd3c5a40b13e8345ab
Felipe F Rocha <felipefonsecarocha@gmail.com>
2019-05-25 12:49:05 -0300

    Comments to this component

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-class Hello extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello Component</h1>
-        <h2>Coneúdo</h2>
-      </div>
-    );
-  }
-}
+/* Statefull component - Lifecicles control and better resolution for states*/
+// class Hello extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello Component</h1>
+//         <h2>Coneúdo Statefull</h2>
+//       </div>
+//     );
+//   }
+// }
 
+/*Stateless - dumb components without states but really fast renders */
+function Hello() {
+  return (
+    <div>
+      <h1>Hello Component</h1>
+      <h2>Coneúdo Stateless</h2>
+    </div>
+  );
+}
 ReactDOM.render(<Hello />, document.getElementById("root"));
