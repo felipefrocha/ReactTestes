24fc7499eacce3362fd0419c473cb8e281e14abd
Felipe F Rocha <felipefonsecarocha@gmail.com>
2019-05-25 13:43:19 -0300

    Component rendering dynamic content

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-/*Stateless - JS insted of JSX*/
+/*Stateless - Return values dinamicaly*/
+
+const getRandomValues = () => {
+  const randomNumber = Math.floor(Math.random() * 5);
+  return randomNumber;
+};
+
+const displayImage = () => {
+  return (
+    <div>
+      <h3>Boa Vencestes!!!</h3>
+      <img src="https://i.giphy.com/media/nURpjDx4ac8Xm/giphy.webp" />
+    </div>
+  );
+};
+
+function header(num) {
+  return <h2>Teste de função dinâmica{num}</h2>;
+}
+
 function Hello() {
-  return React.createElement(
-    "div",
-    null,
-    React.createElement("h1", null, "Hello Component"),
-    React.createElement("h2", null, "Conte\xFAdo JS direto sem babel")
+  const num = getRandomValues();
+  const finalElement = <h4>Acaba Aqui</h4>;
+  if (num === 2) {
+    return displayImage();
+  }
+  return (
+    <div>
+      {num !== 2 && header(num)} {finalElement}
+    </div>
   );
 }
 ReactDOM.render(<Hello />, document.getElementById("root"));
