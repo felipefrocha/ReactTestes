abc2a0706865524894a10a9c2bcc3f745c7c78fd
Felipe F Rocha <felipefonsecarocha@gmail.com>
2019-05-26 11:44:42 -0300

    General component

--- index.js
+++ index.js
@@ -1,33 +1,3 @@
-/*Stateless - Return values dinamicaly*/
+import App from "./App";
 
-const getRandomValues = () => {
-  const randomNumber = Math.floor(Math.random() * 5);
-  return randomNumber;
-};
-
-const displayImage = () => {
-  return (
-    <div>
-      <h3>Boa Vencestes!!!</h3>
-      <img src="https://i.giphy.com/media/nURpjDx4ac8Xm/giphy.webp" />
-    </div>
-  );
-};
-
-function header(num) {
-  return <h2>Teste de função dinâmica{num}</h2>;
-}
-
-function Hello() {
-  const num = getRandomValues();
-  const finalElement = <h4>Acaba Aqui</h4>;
-  if (num === 2) {
-    return displayImage();
-  }
-  return (
-    <div>
-      {num !== 2 && header(num)} {finalElement}
-    </div>
-  );
-}
-ReactDOM.render(<Hello />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
